2fc2fbb48cd04ec782ca082820c29098dcf88fea
dkimd5 <dkimd5@mail.ru>
2022-06-21 14:00:05 +0300

    Create Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react'
+import './Header.css';
+
+type Props = {}
+
+const Header:FC<Props> = (props) => {
+  return (
+    <div className='header'>Header</div>
+  )
+}
+
+export default Header;
