be3849b68a6f6a94c232d457ee905d70d1752d23
dkimd5 <dkimd5@mail.ru>
2022-06-22 13:28:18 +0300

    Add BookList component to App, Display state data at main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header'
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import BooksList from "components/BooksList/BooksList";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
+      <BooksList />
     </div>
   );
 }
--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -1,9 +1,21 @@
 import React, { FC } from "react";
+import { useAppDispatch, useAppSelector } from "app/hooks";
 
 type Props = {};
 
 const BooksList: FC<Props> = (props) => {
-  return <div>BooksList</div>;
+  const booksArray = useAppSelector((state) => state.books.booksArray);
+
+  return (
+    <div>
+      {booksArray.map((book) => (
+        <div>
+          <h1>{book.name}</h1>
+          <span>{book.author}</span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default BooksList;
