21b10e20b9050f5c9ec5bc975d385d5006b1f9a0
dkimd5 <dkimd5@mail.ru>
2022-06-23 13:52:31 +0300

    Add createApi for book slice

--- src/features/books/books-slice.ts
+++ src/features/books/books-slice.ts
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const BOOKS_API_KEY = process.env.REACT_APP_LIBRARY_API_KEY;
+const BOOKS_API_KEY: string = process.env.REACT_APP_LIBRARY_API_KEY!;
 
 interface Book {
   id: string;
@@ -12,24 +12,44 @@ interface Book {
   };
 }
 
-interface BooksState {
-  booksArray: { name: string; author: string }[];
-}
-
-const initialState: BooksState = {
-  booksArray: [
-    { name: "Сага о Форсайтах", author: "John Galsworthy" },
-    { name: "Martin Iden", author: "Jack London" },
-  ],
-};
-
-const booksSlice = createSlice({
-  name: "books",
-  initialState,
-  reducers: {
-    getBooks: (state) => {},
+const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `https://www.googleapis.com/books/v1/volumes?key=${BOOKS_API_KEY}`,
+    // prepareHeaders: (headers) => {
+    //   headers.set("b-api-key", BOOKS_API_KEY);
+    //   return headers;
+    // },
+  }),
+  endpoints(builder) {
+    return {
+      fetchBooks: builder.query<Book[], number | void>({
+        query(limit = 30) {
+          return `&maxResults=${limit}`;
+        },
+      }),
+    };
   },
 });
 
-export const { getBooks } = booksSlice.actions;
-export default booksSlice.reducer;
+// interface BooksState {
+//   booksArray: { name: string; author: string }[];
+// }
+
+// const initialState: BooksState = {
+//   booksArray: [
+//     { name: "Сага о Форсайтах", author: "John Galsworthy" },
+//     { name: "Martin Iden", author: "Jack London" },
+//   ],
+// };
+
+// const booksSlice = createSlice({
+//   name: "books",
+//   initialState,
+//   reducers: {
+//     getBooks: (state) => {},
+//   },
+// });
+
+// export const { getBooks } = booksSlice.actions;
+// export default booksSlice.reducer;
