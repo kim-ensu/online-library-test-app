a9e33774fad3b2bd6a2802a47976cf8191093e50
dkimd5 <dkimd5@mail.ru>
2022-06-23 09:06:40 +0300

    Add book interface

--- src/features/books/books-slice.ts
+++ src/features/books/books-slice.ts
@@ -1,23 +1,35 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const BOOKS_API_KEY = process.env.REACT_APP_LIBRARY_API_KEY;
+
+interface Book {
+  id: string;
+  name: string;
+  author: string;
+  image: {
+    url: string;
+  };
+}
 
 interface BooksState {
-  booksArray: {name: string, author: string}[];
+  booksArray: { name: string; author: string }[];
 }
 
 const initialState: BooksState = {
   booksArray: [
-    {name: "Сага о Форсайтах", author: "John Galsworthy"},
-    {name: "Martin Iden", author: "Jack London"}
-  ]
-}
+    { name: "Сага о Форсайтах", author: "John Galsworthy" },
+    { name: "Martin Iden", author: "Jack London" },
+  ],
+};
 
 const booksSlice = createSlice({
   name: "books",
   initialState,
   reducers: {
-    getBooks: state => {}
-  }
-})
+    getBooks: (state) => {},
+  },
+});
 
 export const { getBooks } = booksSlice.actions;
 export default booksSlice.reducer;
