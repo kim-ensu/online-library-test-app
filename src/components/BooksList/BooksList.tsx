17eb6d3bf2e7ff25f46e3d8f49ab3ec7817b64ed
dkimd5 <dkimd5@mail.ru>
2022-06-25 12:04:29 +0300

    Add receive books data from store at BooksList

--- src/components/BooksList/BooksList.tsx
+++ src/components/BooksList/BooksList.tsx
@@ -1,11 +1,29 @@
 import React, { FC } from "react";
+import { useAppSelector } from "app/hooks";
 
 type Props = {};
 
 const BooksList: FC<Props> = (props) => {
+  const booksList = useAppSelector((state) => state.books.booksArray);
+
   return (
     <>
-      <h1>Search and read your favourite books</h1>
+      {booksList.length ? (
+        <h1>Search and read your favourite books</h1>
+      ) : (
+        booksList.map((book) => (
+          <div key={book.id}>
+            <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
+            <span>{book.volumeInfo.categories[0]}</span>
+            <span>{book.volumeInfo.title}</span>
+            <ul>
+              {book.volumeInfo.authors.map((author) => (
+                <li>{author}</li>
+              ))}
+            </ul>
+          </div>
+        ))
+      )}
     </>
   );
 };
