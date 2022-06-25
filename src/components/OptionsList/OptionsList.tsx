9d06598077b3b341e94a5c6b3e1188824e0c75ac
dkimd5 <dkimd5@mail.ru>
2022-06-25 10:20:45 +0300

    Change OptionsList considering props

--- src/components/OptionsList/OptionsList.tsx
+++ src/components/OptionsList/OptionsList.tsx
@@ -1,10 +1,38 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./OptionsList.css";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
-type Props = {};
+type Props = {
+  id: string;
+  label: string;
+  values: Array<string>;
+};
 
-const OptionsList: FC<Props> = (props) => {
-  return <div>OptionsList</div>;
+const OptionsList: FC<Props> = ({ id, label, values }) => {
+  const [currentValue, setCurrentValue] = useState<string>(values[0]);
+
+  const handleChange = (event: SelectChangeEvent): void => {
+    setCurrentValue(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor={id}>${label}</label>
+      <FormControl id={id} sx={{ m: 1, minWidth: 120, backgroundColor: "white" }}>
+        <Select
+          value={currentValue}
+          onChange={handleChange}
+          displayEmpty
+          inputProps={{ "aria-label": "Without label" }}>
+          {values.map((value) => (
+            <MenuItem value={value}>${value}</MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
 };
 
 export default OptionsList;
