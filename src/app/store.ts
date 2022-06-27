b7c40ccdba233b136bc7ddda0fa4960fe308f650
dkimd5 <dkimd5@mail.ru>
2022-06-27 15:26:49 +0300

    Create slice for search values

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import booksReducer from "../features/books/books-slice";
 import { apiSlice } from "features/books/books-api-slice";
+import booksSearchFields from "features/books/books-search-fields";
 
 export const store = configureStore({
   reducer: {
     books: booksReducer,
+    booksSearchFields: booksSearchFields,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => {
---
+++ src/features/books/books-search-fields.ts
@@ -0,0 +1,32 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { ISearchFields } from "interfaces";
+
+const initialState: ISearchFields = {
+  searchField: "",
+  category: "all",
+  sortingBy: "relevance",
+  booksLoaded: 30,
+};
+
+const booksSearchFieldsSlice = createSlice({
+  name: "booksSearchFields",
+  initialState,
+  reducers: {
+    setSearchField: (state, action: PayloadAction<string>) => {
+      state.searchField = action.payload;
+    },
+    setCategory: (state, action: PayloadAction<string>) => {
+      state.category = action.payload;
+    },
+    setOrderBy: (state, action: PayloadAction<string>) => {
+      state.sortingBy = action.payload;
+    },
+    setMaxLoaded: (state, action: PayloadAction<number>) => {
+      state.booksLoaded = action.payload;
+    },
+  },
+});
+
+export const { setSearchField, setCategory, setOrderBy, setMaxLoaded } =
+  booksSearchFieldsSlice.actions;
+export default booksSearchFieldsSlice.reducer;
