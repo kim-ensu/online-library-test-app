3d9c440e810e5c9b9b02889c9a4f2caf40be0af0
dkimd5 <dkimd5@mail.ru>
2022-06-27 14:09:06 +0300

    Create LoadMoreButton component

---
+++ src/components/LoadMoreButton/LoadMoreButton.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import "./LoadMoreButton.css";
+
+type Props = {};
+
+const LoadMoreButton: FC<Props> = (props) => {
+  return <div>LoadMoreButton</div>;
+};
+
+export default LoadMoreButton;
