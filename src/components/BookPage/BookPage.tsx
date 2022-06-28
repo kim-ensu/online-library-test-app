6bfe68d8fb3aefb37627867c83bd17eef02deedb
dkimd5 <dkimd5@mail.ru>
2022-06-28 17:58:50 +0300

    Select book info from store to bookpage

--- src/components/BookPage/BookPage.tsx
+++ src/components/BookPage/BookPage.tsx
@@ -1,9 +1,16 @@
+import { useAppSelector } from "app/hooks";
 import React, { FC } from "react";
+import { useParams } from "react-router-dom";
 import "./BookPage.css";
 
 type Props = {};
 
 const BookPage: FC<Props> = (props) => {
+  let params = useParams();
+  const bookinfo = useAppSelector((state) =>
+    state.books.booksArray.find((item) => item.id === params.name)
+  );
+
   return (
     <div className="bookpage">
       <div className="bookpage-image-container">
