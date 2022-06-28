4b2edd689212a7848e85e456ef7d6f033c8a6330
dkimd5 <dkimd5@mail.ru>
2022-06-28 17:54:04 +0300

    Add layout for bookpage

--- src/components/BookPage/BookPage.tsx
+++ src/components/BookPage/BookPage.tsx
@@ -4,7 +4,19 @@ import "./BookPage.css";
 type Props = {};
 
 const BookPage: FC<Props> = (props) => {
-  return <div>BookPage</div>;
+  return (
+    <div className="bookpage">
+      <div className="bookpage-image-container">
+        <img src="" alt="" />
+      </div>
+      <div className="bookpage-info-container">
+        <span className="bookpage-categories"></span>
+        <h1 className="bookpage-title"></h1>
+        <span className="bookpage-authors"></span>
+        <p className="bookpage-description"></p>
+      </div>
+    </div>
+  );
 };
 
 export default BookPage;
