a892c7eb61030bc6610a0216a7287a1c90636d38
dkimd5 <dkimd5@mail.ru>
2022-06-28 17:46:03 +0300

    Wrap cards into Link

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import "./BookCard.css";
+import { Link } from "react-router-dom";
 
 type Props = {
   id: string;
@@ -16,32 +17,34 @@ type Props = {
 const BookCard: FC<Props> = ({ id, bookInfo: { imageLinks, title, categories, authors } }) => {
   return (
     <li key={id} className="bookcard">
-      {imageLinks === undefined ? (
-        <i>PLACEHOLDER</i>
-      ) : (
-        <img className="bookcard-img" src={imageLinks.thumbnail} alt={title} />
-      )}
-      <div className="bookcard-text-container">
-        {categories === undefined ? (
-          <div></div>
+      <Link className="bookcard-link" to={"/book/" + id}>
+        {imageLinks === undefined ? (
+          <i>PLACEHOLDER</i>
         ) : (
-          <span className="bookcard-category">{categories[0]}</span>
+          <img className="bookcard-img" src={imageLinks.thumbnail} alt={title} />
         )}
+        <div className="bookcard-text-container">
+          {categories === undefined ? (
+            <div></div>
+          ) : (
+            <span className="bookcard-category">{categories[0]}</span>
+          )}
 
-        <span className="bookcard-title">{title}</span>
+          <span className="bookcard-title">{title}</span>
 
-        {authors === undefined ? (
-          <div></div>
-        ) : (
-          <ul>
-            {authors.map((author, index) => (
-              <li className="bookcard-author" key={id + author}>
-                {author}
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
+          {authors === undefined ? (
+            <div></div>
+          ) : (
+            <ul>
+              {authors.map((author, index) => (
+                <li className="bookcard-author" key={id + author}>
+                  {author}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </Link>
     </li>
   );
 };
